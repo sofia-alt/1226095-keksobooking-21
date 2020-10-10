e462598e50d04ae289b353fc94e00432869ea30f
Sofia <medi61200@mail.ru>
2020-10-10 03:08:15 +0300

    popup content

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const TITLES = [`Title1`, `Title2`, `Title3`, `Title4`, `Title5`];
 const TIMES = [`12:00`, `13:00`, `14:00`];
 const TYPES = [`palace`, `flat`, `house`, `bungalow`];
 const PRISES = [1000, 2000, 3000, 4000];
-const FEATURES = [`1wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
+const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
 
 const quantityPhotos = 3;
 const pinsContainer = document.querySelector(`.map__pins`);
@@ -72,7 +72,7 @@ const getFeatures = (features) => {
 const getPhotos = () => {
   let photos = [];
 
-  for (let i = 1; i <= Math.random(0, quantityPhotos); i++) {
+  for (let i = 0; i <= Math.random(0, quantityPhotos); i++) {
     let photo = `http://o0.github.io/assets/images/tokyo/hotel${i}.jpg`;
     photos.push(photo);
   }
@@ -114,8 +114,36 @@ const getPopup = (pins) => {
   const pin = pins[0];
   var popupElement = popupTemplate.cloneNode(true);
 
-  popupElement.querySelector(`.popup__title`).textContent = pin.title;
-  popupElement.querySelector(`.popup__text--address`).textContent = pin.address;
+  popupElement.querySelector(`.popup__title`).textContent = pin.offer.title;
+  popupElement.querySelector(`.popup__text--address`).textContent = pin.offer.address;
+  popupElement.querySelector(`.popup__text--price`).textContent = `${pin.offer.price}₽/ночь`;
+
+  const localisationType = () => {
+    let pinLocalisationType = pin.offer.type;
+    if (pin.offer.type === `flat`) {
+      pinLocalisationType = `Квартира`;
+    }
+
+    if (pin.offer.type === `bungalow`) {
+      pinLocalisationType = `Бунгало`;
+    }
+
+    if (pin.offer.type === `house`) {
+      pinLocalisationType = `Дом`;
+    }
+
+    if (pin.offer.type === `palace`) {
+      pinLocalisationType = `Дворец`;
+    }
+    return pinLocalisationType;
+  };
+
+  popupElement.querySelector(`.popup__type`).textContent = localisationType();
+  popupElement.querySelector(`.popup__text--capacity`).textContent = `${pin.offer.rooms} комнаты для ${pin.offer.guests} гостей`;
+  popupElement.querySelector(`.popup__text--time`).textContent = `Заезд после ${pin.offer.checkin}, выезд до ${pin.offer.checkout}`;
+  // popupElement.querySelector(`.popup__features`).textContent = pin.offer.features;
+  popupElement.querySelector(`.popup__description`).textContent = pin.offer.description;
+  // popupElement.querySelector(`.popup__photos`).textContent = pin.offer.photos;
 
   mapBlock.appendChild(popupElement);
 };
